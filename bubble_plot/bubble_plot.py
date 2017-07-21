9243ea8fcb6bce38dad8b8605031e411a33a2926
shirmeir <meir.shir86@gmail.com>
2017-07-21 21:37:19 +0300

    Update bubble_plot.py

--- bubble_plot/bubble_plot.py
+++ bubble_plot/bubble_plot.py
@@ -32,16 +32,16 @@ def bubble_plot(df, x, y, ordered_x_values=None, ordered_y_values=None, bins_x=1
         maximal_bubble_size /= 2
     size_factor = maximal_bubble_size/count_table.max().max()
     count_table_long = pd.melt(count_table.reset_index(), id_vars=x)
-    x_values_dict = {x:i for i, x in enumerate(count_table.index.values)} \
+    x_values_dict = {x:i for i, x in enumerate(ordered_x_values)} \
         if not x_is_numeric else {xx:get_point(xx) for xx in ordered_x_values}
-    y_values_dict = {x:i for i, x in enumerate(count_table.columns)} \
+    y_values_dict = {x:i for i, x in enumerate(ordered_y_values)} \
         if not y_is_numeric else {xx: get_point(xx) for xx in ordered_y_values}
     count_table_long[x] = count_table_long[x].map(x_values_dict)
     count_table_long[y] = count_table_long[y].map(y_values_dict)
     xticks = np.arange(count_table.shape[0]) if not x_is_numeric else [get_point(xx) for xx in ordered_x_values]
     yticks = np.arange(count_table.shape[1]) if not y_is_numeric else [get_point(xx) for xx in ordered_y_values]
-    xticklabels = count_table.index.values if not x_is_numeric else [get_point(xx) for xx in ordered_x_values]
-    yticklabels = count_table.columns if not y_is_numeric else [get_point(xx) for xx in ordered_y_values]
+    xticklabels = ordered_x_values if not x_is_numeric else [get_point(xx) for xx in ordered_x_values]
+    yticklabels = ordered_y_values if not y_is_numeric else [get_point(xx) for xx in ordered_y_values]
     plt.scatter(count_table_long[x], count_table_long[y], s=size_factor*count_table_long['value'],
                 c=count_table_long['value'], cmap='cool')
     plt.xticks(xticks, xticklabels,fontsize=fontsize)
