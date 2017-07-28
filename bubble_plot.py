8d69768470e9e82fd3938aac5e67b77545501050
shirmeir <meir.shir86@gmail.com>
2017-07-28 14:34:19 +0300

    Update bubble_plot.py

--- bubble_plot.py
+++ bubble_plot.py
@@ -64,3 +64,55 @@ def bubble_plot(df, x, y, ordered_x_values=None, ordered_y_values=None, bins_x=1
     plt.xlabel(x, fontsize=fontsize)
     plt.ylabel(y, fontsize=fontsize)
     plt.title("{} vs {} ".format(y,x),fontsize=fontsize+4);
+
+    
+    
+def bubble_plot2(df, x, y, z_boolean=None, ordered_x_values=None, ordered_y_values=None, bins_x=10,
+            bins_y=10, fontsize=16, figsize=(15,10), maximal_bubble_size=5000,):
+    """
+    :param df: dataframe
+    :param x:  name of first numerical/categorical field (string) (for x-axis)
+    :param y: name of second numerical/categorical field (string) (for y-axis)
+    :param ordered_x_values: the values we would like to map from x categorical variable 
+    according to the order we would like to present them
+    :param ordered_y_values: the values we would like to map from the y categorical variable 
+    according to the order we would like to present them
+    :param bins_x: the bins for x values if x is numberic
+    :param bins_y: the bins for y values if y is numberic
+    :param normalization_by_all: True - shows joint distribution p(x,y), False - shows conditional distribution p(y|x)
+    :param maximal_bubble_size: if the bubbles are too big or too small this is the parameter you should change!
+    :param log: whether to apply log on the count (influence the size of the bubbles)
+    :return: nice bubble plot :)
+    """
+    plt.figure(figsize=figsize)
+    x_is_numeric = df[x].dtype in (float, int) and ordered_x_values is None
+    y_is_numeric = df[y].dtype in (float, int) and ordered_y_values is None 
+    count_table = pd.concat([pd.cut(df[x], bins=bins_x) if x_is_numeric else df[x],
+                             pd.cut(df[y], bins=bins_y) if y_is_numeric else df[y]], axis=1)
+    count_table = count_table.groupby(x)[y].value_counts().unstack().fillna(0)
+    ordered_x_values = count_table.index.values if ordered_x_values is None else ordered_x_values
+    ordered_y_values = count_table.columns if ordered_y_values is None else ordered_y_values
+
+    count_table = pd.concat([pd.cut(df[x], bins=bins_x) if x_is_numeric else df[x],
+                         pd.cut(df[y], bins=bins_y) if y_is_numeric else df[y], df[z_boolean]], axis=1)
+    count_table = count_table.groupby([x,z_boolean])[y].value_counts().unstack().fillna(0)
+    count_table = count_table.unstack()
+    count_table_long = pd.melt(count_table.reset_index(), id_vars=x)
+    z_boolean_values = count_table_long[z_boolean].unique()
+    ratio = pd.DataFrame({'ratio':count_table_long.set_index([x,y,z_boolean]).unstack()['value'][z_boolean_values[1]] / (
+    count_table_long.set_index([x,y,z_boolean]).unstack()['value'].sum(axis=1) )})
+    count_table_long = count_table_long.set_index(['age','hours-per-week'])[['value']].merge(ratio, left_index=True, right_index=True).reset_index()
+    x_values_dict = {x:i for i, x in enumerate(ordered_x_values)} \
+        if not x_is_numeric else {xx:get_point(xx) for xx in ordered_x_values}
+    y_values_dict = {x:i for i, x in enumerate(ordered_y_values)} \
+        if not y_is_numeric else {xx: get_point(xx) for xx in ordered_y_values}
+    count_table_long[x] = count_table_long[x].map(x_values_dict)
+    count_table_long[y] = count_table_long[y].map(y_values_dict)
+    plt.scatter(count_table_long[x], count_table_long[y], s=count_table_long['value'],
+                c=count_table_long['ratio'], 
+                cmap='cool')
+    plt.xticks(xticks, xticklabels,fontsize=fontsize)
+    plt.yticks(yticks, yticklabels,fontsize=fontsize)
+    plt.xlabel(x, fontsize=fontsize)
+    plt.ylabel(y, fontsize=fontsize)
+    plt.title("{} vs {} ".format(y,x),fontsize=fontsize+4);
